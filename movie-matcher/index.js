71ca3bffdae7ca9448b6c78c8e6f97a144f10664
Timothy Millward <silhouettetechnician@gmail.com>
2019-04-24 23:17:00 +0100

    index.js built

---
+++ movie-matcher/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+// the above is calling the express function allowing it to be used
+const bodyParser = require('body-parser')
+// a bodyParser is required to read the body responses
+const mongoose = require('mongoose')
+
+const env = require('./config/environment')
+const router = require('./config/routes')
+const errorHandler = require('./lib/errorHandler')
+
+mongoose.connect(env.db.development , { useNewUrlParser: true })
+console.log('hello')
+// the use of middleware makes sure we  will recieve a response
+app.use(bodyParser.json())
+app.use('/api', router)
+app.use(errorHandler)
+
+app.listen(env.port, () => console.log(`App is listening on port ${env.port}`))
+
+module.exports = app
--- movie-matcher/webpack.config.js
+++ movie-matcher/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve('dist')
+    path: path.resolve('dist'),
+    publicPatch: '/'
   },
   module: {
     rules: [
